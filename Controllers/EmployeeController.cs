81d9c1a1b2fe24d6a6b1f96525d84af42bba6b04
admin <sk405044@gmail.com>
2022-05-02 22:25:38 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Model;
+namespace WebApplication4.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = dbContext.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
